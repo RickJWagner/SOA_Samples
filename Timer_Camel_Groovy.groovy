0bcf6eda18f2bc3e5f55ac7c3cd28e11f9c687cb
Rick <rwagner@redhat.com>
2013-03-19 10:00:55 -0500

    First Groovy Camel samples

---
+++ Timer_Camel_Groovy.groovy
@@ -0,0 +1,17 @@
+@Grab('org.apache.camel:camel-core:2.10.0')
+@Grab('org.slf4j:slf4j-simple:1.6.6')
+import org.apache.camel.*
+import org.apache.camel.impl.*
+import org.apache.camel.builder.*
+
+def camelContext = new DefaultCamelContext()
+camelContext.addRoutes(new RouteBuilder() {
+    def void configure() {
+        from("timer://jdkTimer?period=3000")
+            .to("log://camelLogger?level=INFO")
+    }
+})
+camelContext.start()
+
+addShutdownHook{ camelContext.stop() }
+synchronized(this){ this.wait() }
