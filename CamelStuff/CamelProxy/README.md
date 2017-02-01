b0018ee74c9b6a1f0ce2765420d31ab2e202cd0c
Rick Wagner <RickJWag@yahoo.com>
2017-02-01 13:29:30 -0600

    Camel Spring Proxy run from mvn exec:java

---
+++ CamelStuff/CamelProxy/README.md
@@ -0,0 +1,5 @@
+
+To run this class after building, use a command line like this:
+
+ mvn exec:java  -Dexec.mainClass="org.sample.MainClass"
+
---
+++ CamelStuff/CamelProxy/src/main/java/org/sample/MainClass.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+import org.apache.camel.spring.Main;
+
+public class MainClass {
+
+	public static void main(String[] args) throws Exception {
+		
+		new Main().run();
+
+	}
+
+}
